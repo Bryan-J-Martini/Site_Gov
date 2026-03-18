65fd563fe48bdfd94c66f96fda76eeb5fd09a255
Remédio <danielremedio2000@gmail.com>
2026-03-18 16:49:27 -0300

    Estabelecendo IDE

---
+++ Site_Gov/Site_Gov/Site_Gov/Login.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata;
+
+namespace Site_Gov
+{
+    internal class Login
+    {
+        private string UserName { get; set }
+        private string Password { get; set; }
+
+        // Métodos 
+
+        private Validador(string username, string password)
+        {
+            this.UserName = username;
+            this.Password = password;
+        }
+
+    }
+    
+}
---
+++ Site_Gov/Site_Gov/Site_Gov/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Site_Gov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
